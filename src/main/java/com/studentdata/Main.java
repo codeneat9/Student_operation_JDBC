6f2ac674b9f43faffe0512770ae8b3653ba5ffb0
codeneat9 <yashwithbehara18@gmail.com>
2025-04-21 23:33:37 +0530

    Displaying the menu/functions to the user

--- src/main/java/com/studentdata/Main.java
+++ src/main/java/com/studentdata/Main.java
@@ -8,3 +8,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Add Student");
+            System.out.println("2. Display Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Update Student");
+            System.out.println("5. Delete Student");
+            System.out.println("6. Exit");
+
+            System.out.print("Enter choice: ");
