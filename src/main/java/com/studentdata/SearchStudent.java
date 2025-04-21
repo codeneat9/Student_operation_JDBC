689d9efe173c14dcaa597f7a951e6b8f39f57da5
codeneat9 <yashwithbehara18@gmail.com>
2025-04-21 23:47:08 +0530

    Taking the PRN as the input for searching

--- src/main/java/com/studentdata/SearchStudent.java
+++ src/main/java/com/studentdata/SearchStudent.java
@@ -5,3 +5,8 @@ import java.util.Scanner;
 
 public class SearchStudent {
 
+    public static void searchStudent() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter PRN/Name/Position to search: ");
+        String searchQuery = sc.nextLine();
+
