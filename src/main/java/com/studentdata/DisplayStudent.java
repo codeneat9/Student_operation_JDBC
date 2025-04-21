b57d8b6d0b4ce0167291a6756508141c302aff72
codeneat9 <yashwithbehara18@gmail.com>
2025-04-21 23:45:28 +0530

    Displaying the student details after connection

--- src/main/java/com/studentdata/DisplayStudent.java
+++ src/main/java/com/studentdata/DisplayStudent.java
@@ -10,3 +10,11 @@ public class DisplayStudent {
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
 
+            while (rs.next()) {
+                System.out.println("PRN: " + rs.getString("prn"));
+                System.out.println("Name: " + rs.getString("name"));
+                System.out.println("Batch: " + rs.getString("batch"));
+                System.out.println("Position: " + rs.getString("position"));
+                System.out.println("-----------------------------------");
+            }
+
