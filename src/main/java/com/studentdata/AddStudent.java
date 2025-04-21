b178a7b594a2dfe5c92e075eaf5ed5d2ef89f3c4
codeneat9 <yashwithbehara18@gmail.com>
2025-04-21 23:36:47 +0530

    Inputting details of students to add

--- src/main/java/com/studentdata/AddStudent.java
+++ src/main/java/com/studentdata/AddStudent.java
@@ -8,3 +8,13 @@ public class AddStudent {
     public static void addStudent() {
         Scanner sc = new Scanner(System.in);
 
+        // Getting student details from user
+        System.out.println("Enter PRN: ");
+        String prn = sc.nextLine();
+        System.out.println("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter Batch: ");
+        String batch = sc.nextLine();
+        System.out.println("Enter Position: ");
+        String position = sc.nextLine();
+
