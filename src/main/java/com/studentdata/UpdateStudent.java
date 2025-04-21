8ccde56328c841be1c4915f87920171d58fe0f56
codeneat9 <yashwithbehara18@gmail.com>
2025-04-21 23:50:28 +0530

    Inputting the details to be updated to

--- src/main/java/com/studentdata/UpdateStudent.java
+++ src/main/java/com/studentdata/UpdateStudent.java
@@ -8,3 +8,13 @@ public class UpdateStudent {
     public static void updateStudent() {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter PRN to update: ");
+        String prn = sc.nextLine();
+
+        System.out.println("Enter new Name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter new Batch: ");
+        String batch = sc.nextLine();
+        System.out.println("Enter new Position: ");
+        String position = sc.nextLine();
+
