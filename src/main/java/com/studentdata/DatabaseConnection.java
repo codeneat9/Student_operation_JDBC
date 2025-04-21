ec5646c7c9909c075e439ee5512a7db0a14f6b09
codeneat9 <yashwithbehara18@gmail.com>
2025-04-21 23:40:47 +0530

    Establishing connection with JDBC

--- src/main/java/com/studentdata/DatabaseConnection.java
+++ src/main/java/com/studentdata/DatabaseConnection.java
@@ -2,3 +2,8 @@ package src.main.java.com.studentdata;
 
 import java.sql.*;
 
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String USER = "root";  // Your MySQL username
+    private static final String PASSWORD = "sql123(randomly given, to hidde my privacy)";  // Your MySQL password
+
