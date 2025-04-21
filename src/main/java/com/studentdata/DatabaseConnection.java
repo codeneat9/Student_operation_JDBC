879fee1f7a25060070882143b71dd42bb33293b9
codeneat9 <yashwithbehara18@gmail.com>
2025-04-21 23:41:17 +0530

    Handling SQL connection exception

--- src/main/java/com/studentdata/DatabaseConnection.java
+++ src/main/java/com/studentdata/DatabaseConnection.java
@@ -7,3 +7,15 @@ public class DatabaseConnection {
     private static final String USER = "root";  // Your MySQL username
     private static final String PASSWORD = "sql123(randomly given, to hidde my privacy)";  // Your MySQL password
 
+    // Method to establish the connection to the database
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");  // MySQL Driver class
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found.", e);
+        } catch (SQLException e) {
+            throw new SQLException("Error while connecting to database.", e);
+        }
+    }
+}
